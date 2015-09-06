36f6eeb271c61b3a8389fbec3b8c61d4cc46803c
Josh Cough <jcough@localytics.com>
2015-09-05 21:25:50 -0400

    progress

--- src/Scrabble/Search.hs
+++ src/Scrabble/Search.hs
@@ -1,16 +1,22 @@
 module Scrabble.Search where
 
-import Data.List hiding (or, and)
+import Data.Char (toLower, toUpper)
 import System.Random.Shuffle
-import Prelude hiding (Word, or, and)
+import Prelude hiding (Word, or, and, all)
 import Scrabble.Types
+import qualified Data.List as List
 
-dictionary = readFile "dict/en.txt"
+dictionary :: IO [String]
+dictionary = lines <$> readFile "dict/en.txt"
 
 type Search1 = Tray -> Word -> Bool
 
-runSearch1 :: Tray -> Search1 -> [Word] -> [Word]
-runSearch1 t s = filter (s t)
+cheat :: Search1 -> Tray -> IO [Word]
+cheat search tray = runSearch1 search tray <$> dictionary
+
+-- Run a search on a whole dictionary of words
+runSearch1 :: Search1 -> Tray -> [Word] -> [Word]
+runSearch1 s t = filter (s t)
 
 or :: Search1 -> Search1 -> Search1
 or s1 s2 r w = s1 r w || s2 r w
@@ -33,4 +39,27 @@ any = combine False or
 all :: [Search1] -> Search1
 all = combine True and
 
+lows :: String -> String
+lows = List.sort . fmap toLower
+
+-- TODO: be careful with what this means...
+-- if there are dups in the tray, does the word also need dups?
+containsAll :: Search1
+containsAll = contains' List.all
+
+containsAny :: Search1
+containsAny = contains' List.any
+
+contains' :: ((Char -> Bool) -> [Char] -> Bool) -> Search1
+contains' f t w = f (\c -> elem c w') (lows t) where w' = lows w
+
+containsOnly :: Search1
+containsOnly t w = lows t == lows w
+
+containsNone :: Search1
+containsNone t = not . containsAny t
+
+containsLetterAtPos :: Letter -> Int -> Search1
+containsLetterAtPos l n _ w =
+  if n - 1 > length w then False else w !! n == l
 
