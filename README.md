b0b8dac4d5eacdf7ec48939d6b30d946cd2abad9
Josh Cough <jcough@localytics.com>
2015-09-05 21:41:06 -0400

    no tray needed to search

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Scrabble
+
+    $ cabal repl
+    *Scrabble> cheat (Scrabble.Search.all [containsAny "z", containsLetterAtPos 'w' 4])
+    ["buzzwig","buzzwigs","buzzword","buzzwords","unbowdlerized","zugzwang","zugzwanged","zugzwanging","zugzwangs"]
--- src/Scrabble/Search.hs
+++ src/Scrabble/Search.hs
@@ -5,33 +5,34 @@ import System.Random.Shuffle
 import Prelude hiding (Word, or, and, all)
 import Scrabble.Types
 import qualified Data.List as List
+import Debug.Trace
 
 dictionary :: IO [String]
 dictionary = lines <$> readFile "dict/en.txt"
 
-type Search1 = Tray -> Word -> Bool
+type Search1 = Word -> Bool
 
-cheat :: Search1 -> Tray -> IO [Word]
-cheat search tray = runSearch1 search tray <$> dictionary
+cheat :: Search1 -> IO [Word]
+cheat search = runSearch1 search <$> dictionary
 
 -- Run a search on a whole dictionary of words
-runSearch1 :: Search1 -> Tray -> [Word] -> [Word]
-runSearch1 s t = filter (s t)
+runSearch1 :: Search1 -> [Word] -> [Word]
+runSearch1 s = filter s
 
 or :: Search1 -> Search1 -> Search1
-or s1 s2 r w = s1 r w || s2 r w
+or s1 s2 w = s1 w || s2 w
 
 and :: Search1 -> Search1 -> Search1
-and s1 s2 r w = s1 r w && s2 r w
+and s1 s2 w = s1 w && s2 w
 
 any' :: [Search1] -> Search1
-any' = foldl or (\_ _ -> False)
+any' = foldl or (const False)
 
 all' :: [Search1] -> Search1
-all' = foldl and (\_ _ -> True)
+all' = foldl and (const True)
 
 combine :: Bool -> (Search1 -> Search1 -> Search1) -> [Search1] -> Search1
-combine b f = foldl f (\_ _ -> b)
+combine b f = foldl f (const b)
 
 any :: [Search1] -> Search1
 any = combine False or
@@ -43,23 +44,22 @@ lows :: String -> String
 lows = List.sort . fmap toLower
 
 -- TODO: be careful with what this means...
--- if there are dups in the tray, does the word also need dups?
-containsAll :: Search1
+-- if there are dups in the input, does the word also need dups?
+containsAll :: Tray -> Search1
 containsAll = contains' List.all
 
-containsAny :: Search1
+containsAny :: Tray -> Search1
 containsAny = contains' List.any
 
-contains' :: ((Char -> Bool) -> [Char] -> Bool) -> Search1
+contains' :: ((Char -> Bool) -> [Char] -> Bool) -> Tray -> Search1
 contains' f t w = f (\c -> elem c w') (lows t) where w' = lows w
 
-containsOnly :: Search1
+containsOnly :: Tray -> Search1
 containsOnly t w = lows t == lows w
 
-containsNone :: Search1
+containsNone :: Tray -> Search1
 containsNone t = not . containsAny t
 
 containsLetterAtPos :: Letter -> Int -> Search1
-containsLetterAtPos l n _ w =
-  if n - 1 > length w then False else w !! n == l
+containsLetterAtPos l n w = if n >= length w then False else w !! n == l
 
