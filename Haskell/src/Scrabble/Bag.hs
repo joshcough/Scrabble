d3b406b9cea483421e91d854b6d6f8ebf582347e
Josh Cough <jcough@localytics.com>
2015-09-24 19:29:07 -0400

    Making lines smaller so they can fit on pages better.

--- Haskell/src/Scrabble/Bag.hs
+++ Haskell/src/Scrabble/Bag.hs
@@ -47,13 +47,15 @@ points = [
   ('F',4),('G',2), ('H',4),('I',1),('J',8),
   ('K',5),('L',1), ('M',3),('N',1),('O',1),
   ('P',3),('Q',10),('R',1),('S',1),('T',1),
-  ('U',1),('V',4), ('W',4),('X',8),('Y',4),('Z',10), ('_', 0)]
+  ('U',1),('V',4), ('W',4),('X',8),('Y',4),
+  ('Z',10), ('_', 0)]
 
 pointsMap :: Map Letter Points
 pointsMap = Map.fromList points
 
 distributionWithPoints :: Map Letter (Int,Points)
-distributionWithPoints = Map.intersectionWith (,) distributionMap pointsMap
+distributionWithPoints =
+  Map.intersectionWith (,) distributionMap pointsMap
 
 totalPoints :: Int
 totalPoints = sum $ Map.map (uncurry (*)) distributionWithPoints
--- Haskell/src/Scrabble/Commands/Interpreter.hs
+++ Haskell/src/Scrabble/Commands/Interpreter.hs
@@ -12,10 +12,15 @@ import Scrabble.Search
 import Scrabble.Types
 import Prelude hiding (Word)
 
-interpCommandString :: Game -> String -> Either String CommandResult
-interpCommandString g command = fromString command >>= interpretExp g
+interpCommandString :: Game ->
+                       String ->
+                       Either String CommandResult
+interpCommandString g cmd = fromString cmd >>= interpretExp g
 
-data Move = Move { pointsScored :: Points, remaining :: Tray, boardAfterMove :: ListBoard }
+data Move = Move {
+  pointsScored :: Points
+ ,remaining :: Tray
+ ,boardAfterMove :: ListBoard }
 
 data PrintCommand =
     QueryResult [(Word, Points)]
@@ -29,42 +34,44 @@ data CommandResult =
   | NextPlayer Game
 
 interpretExp :: Game -> ScrabbleExp -> Either String CommandResult
-interpretExp g = f where
-  f Skip                        = return . NextPlayer $ turnOver g
-  f (ShowExp ShowHelp)      = return $ pc   PrintHelp
-  f (ShowExp (ShowBoard b)) = return . pc $ PrintBoard b (gameBoard g)
-  f (ShowExp ShowScores)    = return . pc . PrintScores $ getScores g
-  f (Search search)             =
-    pc . QueryResult <$> interpretSearch search (gameDict g)
-  f (Place pw)                  = MoveResult  <$> g' where
-    g' = applyMove g <$> interpretPut (gameBoard g) t pw
-    t  = playerTray $ currentPlayer g
-  pc = Print
+interpretExp g@(Game (p:ps) board bag dict) = f where
+  f Skip                    = return . NextPlayer $ turnOver g
+  f (ShowExp ShowHelp)      = rPrint   PrintHelp
+  f (ShowExp (ShowBoard b)) = rPrint $ PrintBoard b board
+  f (ShowExp ShowScores)    = rPrint . PrintScores $ getScores g
+  f (Search search)         =
+    Print . QueryResult <$> interpretSearch search dict
+  f (Place pw)              = MoveResult  <$> g' where
+    g' = applyMove g <$> interpretPut board (playerTray p) pw
+  rPrint = return . Print
 
 getScores :: Game -> [(Name, Score)]
 getScores g = getNameAndScore <$> gamePlayers g
 
-interpretSearch :: SearchExp -> Dict -> Either String [(Word, Points)]
-interpretSearch search dict = lookupWithPoints <$> toSearch1 search where
-  lookupWithPoints :: Search1 -> [(Word, Points)]
-  lookupWithPoints search = fmap f (runSearch1 search dict) where
+interpretSearch :: SearchExp ->
+                   Dict      ->
+                   Either String [(Word, Points)]
+interpretSearch search dict = wps <$> toSearch1 search where
+  wps :: Search1 -> [(Word, Points)]
+  wps search = fmap f (runSearch1 search dict) where
     f w = (w,simpleWordPoints w)
 
 interpretPut :: ListBoard -> Tray -> PutWord -> Either String Move
 interpretPut b tray pw = if valid then go else Left errMsg where
   errMsg        = "error: tray missing input letters"
-  valid         = containsAll putLetters trayLetters
   trayLetters   = fmap letter tray
+  valid         = containsAll putLetters trayLetters
   putLetters    = letter <$> tiles pw
-  trayRemainder = fmap fromLetter $ foldl' (flip delete) trayLetters putLetters
+  trayRemainder = fmap fromLetter $
+    foldl' (flip delete) trayLetters putLetters
   go = do (newBoard, score) <- putWord b pw
           return $ Move score trayRemainder newBoard
 
 applyMove :: Game -> Move -> Game
-applyMove g@(Game (p:ps) _ bag d) (Move points remaining updatedBoard) =
-  Game (ps++[player']) updatedBoard bag' d where
+applyMove g@(Game (p:ps) _ bag d) (Move pts tray newBoard) =
+  Game (ps++[player']) newBoard bag' d where
     player'   = Player (playerType p) (playerName p) tray' points'
-    tray'     = take trayLenth bag ++ remaining
+    tray'     = take trayLenth bag ++ tray
     bag'      = drop trayLenth bag
-    points'   = playerScore p + points
-    trayLenth = 7 - length remaining
+    points'   = playerScore p + pts
+    trayLenth = 7 - length tray
