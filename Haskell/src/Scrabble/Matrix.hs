268e084e554f434adfcc489b0c06811a704ae41f
Josh Cough <jcough@localytics.com>
2015-09-24 01:13:00 -0400

    Making lines smaller so they can fit on pages better.

--- Haskell/src/Scrabble/Matrix.hs
+++ Haskell/src/Scrabble/Matrix.hs
@@ -10,16 +10,15 @@ module Scrabble.Matrix
   ) where
 
 import Data.Functor.Compose (Compose(..))
-import Data.Maybe (Maybe)
-import qualified Data.Maybe as Maybe
+import Data.Maybe (Maybe,fromMaybe,catMaybes)
 import Scrabble.Types (Pos(..))
 
 class Vec m where
   before :: Int -> m a -> m a
   after  :: Int -> m a -> m a
 
--- | A generic matrix type 'm', whose rows are represented by the
--- associated 'Row'.
+-- | A generic matrix type 'm', whose rows are
+-- represented by the associated 'Row'.
 class Matrix m where
   type Row m :: * -> *
   elemAt  :: Pos p => (m a) -> p -> Maybe a
@@ -43,17 +42,20 @@ instance Vec [] where
   before = take
   after  = drop . (+1)
 
+inListBounds :: Pos p => p -> Bool
+inListBounds p = x p >= 0 && y p >= 0
+
 instance Matrix ListMatrix where
   type Row ListMatrix = []
-  elemAt  (LM m) p | x >= 0 && y >= 0 = Just (m !! y !! x) where (x,y) = coors p
+  elemAt  (LM m) p | inListBounds p = Just $ m !! y p !! x p
   elemAt  _ _ = Nothing
   row     (LM m) y | y >= 0 = Just $ m !! y
   row     _ _ = Nothing
   col     (LM m) x | x >= 0 = Just $ fmap (!!x) m
   col     _ _ = Nothing
   rows    m   = m
-  cols    m   = LM (Maybe.catMaybes $ fmap (col m) [0..14])
-  above   m p = Maybe.fromMaybe [] $ before (y p) <$> col m (x p)
-  below   m p = Maybe.fromMaybe [] $ after  (y p) <$> col m (x p)
-  leftOf  m p = Maybe.fromMaybe [] $ before (x p) <$> row m (y p)
-  rightOf m p = Maybe.fromMaybe [] $ after  (x p) <$> row m (y p)
+  cols    m   = LM (catMaybes $ fmap (col m) [0..14])
+  above   m p = fromMaybe [] $ before (y p) <$> col m (x p)
+  below   m p = fromMaybe [] $ after  (y p) <$> col m (x p)
+  leftOf  m p = fromMaybe [] $ before (x p) <$> row m (y p)
+  rightOf m p = fromMaybe [] $ after  (x p) <$> row m (y p)
