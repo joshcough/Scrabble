e5b355d45af53a267baecff4819d00eb462a6097
Joshua Cough <joshcough@gmail.com>
2016-02-10 09:08:35 -0500

    websocket game play now at least somewhat playable.

--- web/wai-server/Main.hs
+++ web/wai-server/Main.hs
@@ -18,6 +18,8 @@ import Network.WebSockets
 import Scrabble
 import System.IO.Unsafe
 
+type PlayerName = B.ByteString
+
 -- | Run the web app
 main :: IO ()
 main = do
@@ -26,82 +28,100 @@ main = do
   run 8000 (socketsApp r)
 
 -- | This is the main web app that handles all requests
---   it handles websocket connections itself, and passes
+--   it handles web socket connections itself, and passes
 --   all other requests to the default app
 socketsApp :: IORef Int -> Wai.Application
 socketsApp ref = websocketsOr defaultConnectionOptions wsApp defaultApp where
+
+  -- | The app that handles web socket requests
   wsApp :: ServerApp  -- ServerApp is just this: IORef Int -> PendingConnection -> IO ()
   wsApp pending_conn = do
     i <- readIORef ref
     case i of
-      0 -> connect connection1 pending_conn ref 1
-      1 -> connect connection2 pending_conn ref 2
+      0 -> connect player1 pending_conn ref 1
+      1 -> connect player2 pending_conn ref 2
       _ -> rejectRequest pending_conn "too many players"
 
--- | This is a simple app that just serves up some html and javascript
-defaultApp :: Wai.Application
-defaultApp request respond = go where
-  go = do
-    putStrLn (B.unpack $ Wai.rawPathInfo request)
-    respond $ case Wai.rawPathInfo request of
-      "/"        -> plainIndex
-      "/game.js" -> gameJS
-      bad        -> notFound bad
-  plainIndex    = Wai.responseFile status200 [(hContentType, "text/html")]       "web/wai-server/index.html" Nothing
-  gameJS        = Wai.responseFile status200 [(hContentType, "text/javascript")] "web/wai-server/game.js" Nothing
-  notFound path = Wai.responseLBS  status404 [("Content-Type", "text/plain")] (LB.append "404 - Not Found: " (LB.fromStrict path))
+  -- | This is a simple app that just serves up some html and javascript
+  defaultApp :: Wai.Application
+  defaultApp request respond = go where
+    go = do
+      putStrLn (B.unpack $ Wai.rawPathInfo request)
+      respond $ case Wai.rawPathInfo request of
+        "/"        -> plainIndex
+        "/game.js" -> gameJS
+        bad        -> notFound (LB.fromStrict bad)
+    plainIndex    = Wai.responseFile status200 [(hContentType, "text/html")]       "web/wai-server/index.html" Nothing
+    gameJS        = Wai.responseFile status200 [(hContentType, "text/javascript")] "web/wai-server/game.js" Nothing
+    notFound path = Wai.responseLBS  status404 [("Content-Type", "text/plain")] (LB.append "404 - Not Found: " path)
 
--- | Connect with the incoming websocket request, set up communication with it in general.
-connect :: Show a => MVar Connection -> PendingConnection -> IORef a -> a -> IO ()
-connect convar pending_conn ref i = do
-  conn <- acceptRequest pending_conn
-  writeIORef ref i
-  putMVar convar conn
-  forkPingThread conn 30
-  forever $ receiveMove conn -- listen for moves, forever.
+  -- | Connect with the incoming websocket request, set up communication with it in general.
+  connect :: Show a => MVar (PlayerName, Connection) -> PendingConnection -> IORef a -> a -> IO ()
+  connect convar pending_conn ref i = do
+    conn <- acceptRequest pending_conn
+    writeIORef ref i
+    -- as soon as we connect, they must send their name.
+    name <- receiveData conn :: IO B.ByteString
+    putMVar convar (name, conn)
+    forkPingThread conn 30
+    forever $ receiveMove name conn -- listen for moves, forever.
 
-{-# NOINLINE connection1 #-}
-connection1 :: MVar Connection
-connection1 = unsafePerformIO newEmptyMVar
+  -- | receive a move (and game) from a player, and attempt to apply it
+  --   if it isn't the players turn, or the move results in an error,
+  --   then send the error message back on the connection
+  --   if it succeeds, send the new game state to both players
+  -- TODO: check if it is the players turn.
+  receiveMove :: B.ByteString -> Connection -> IO ()
+  receiveMove pName conn = go where
+    go = do
+      gameAndMove <- receiveData conn
+      case decodeGameAndMove gameAndMove of
+        Right g     -> updateBothClients g
+        Left errMsg -> sendTextData conn (B.pack errMsg)
+    decodeGameAndMove :: B.ByteString -> Either String Game
+    decodeGameAndMove gameAndMove = do
+      (g,m) <- eitherDecode $ LB.fromStrict gameAndMove
+      if (playerName . NE.head $ gamePlayers g) == B.unpack pName
+      then Right () else Left "Not your turn"
+      applyWordPut g m
 
-{-# NOINLINE connection2 #-}
-connection2 :: MVar Connection
-connection2 = unsafePerformIO newEmptyMVar
+-- | send an updated game state to both clients.
+updateBothClients :: Game -> IO ()
+updateBothClients g = do
+  c1 <- connection1
+  c2 <- connection2
+  let json = encode g
+  sendTextData c1 json
+  sendTextData c2 json
+
+-- ===== Horrible mutable state below ===== --
 
 -- | wait for two players to connect
 --   then send them their player names, and the new game.
 gameStartThread :: IO ThreadId
 gameStartThread = forkIO $ do
-  c1 <- readMVar connection1
-  c2 <- readMVar connection2
-  g  <- newGame $ NE.fromList [human "player1", human "player2"]
-  sendTextData c1 ("player1" :: LB.ByteString)
-  sendTextData c2 ("player2" :: LB.ByteString)
+  p1 <- player1Name
+  p2 <- player2Name
+  g  <- newGame $ NE.fromList [human $ B.unpack p1, human $ B.unpack p2]
+  -- this is just a hack until players can enter their name.
   updateBothClients g
 
--- | send an updated game state to both clients.
-updateBothClients :: Game -> IO ()
-updateBothClients g = do
-  c1 <- readMVar connection1
-  c2 <- readMVar connection2
-  let json = encode g
-  sendTextData c1 json
-  sendTextData c2 json
-  return ()
+{-# NOINLINE player1 #-}
+player1 :: MVar (PlayerName, Connection)
+player1 = unsafePerformIO newEmptyMVar
 
--- | receive a move (and game) from a player, and attempt to apply it
---   if it isn't the players turn, or the move results in an error,
---   then send the error message back on the connection
---   if it succeeds, send the new game state to both players
--- TODO: check if it is the players turn.
-receiveMove :: Connection -> IO ()
-receiveMove conn = do
-  gameAndMove <- receiveData conn
-  case decodeGameAndMove gameAndMove of
-    Right g     -> updateBothClients g
-    Left errMsg -> sendTextData conn (B.pack errMsg)
+{-# NOINLINE player2 #-}
+player2 :: MVar (PlayerName, Connection)
+player2 = unsafePerformIO newEmptyMVar
 
-decodeGameAndMove :: LB.ByteString -> Either String Game
-decodeGameAndMove gameAndMove = do
-  (g,m) <- eitherDecode (gameAndMove :: LB.ByteString)
-  applyWordPut g m
+player1Name :: IO PlayerName
+player1Name = readMVar player1 >>= return . fst
+
+player2Name :: IO PlayerName
+player2Name = readMVar player2 >>= return . fst
+
+connection1 :: IO Connection
+connection1 = readMVar player1 >>= return . snd
+
+connection2 :: IO Connection
+connection2 = readMVar player2 >>= return . snd
--- web/wai-server/game.js
+++ web/wai-server/game.js
@@ -1,16 +1,23 @@
+var playerName;
 
 function Client(socket) {
     socket.onopen = function () {
-        console.log("opened web socket")
+      // TODO: need a real way for players to enter their name
+      socket.send("playerNameHere!");
     }
     socket.onclose = function () {
-        console.log("closed web socket")
+      console.log("closed web socket")
     }
     socket.onerror = function (event) {
-        console.log(event)
+      console.log(event)
     }
     socket.onmessage = function (event) {
-      console.log(event)
+      // TODO: here is where magic must happen.
+      // we must have gotten a new game, or an error message
+      // check, and if its a new game, update the ui, and
+      // if its this players turn, let them enter a word.
+      // (check if it's this players turn by their name...
+      // and id...which we still don't have yet. for not thats okay.
       console.log(event.data)
     }
 }
